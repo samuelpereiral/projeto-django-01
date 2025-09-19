6f83664e68f2a73a610550616a67dfdff3c4b2b8
samuelpereiral <samuelpereiral2021@gmail.com>
2025-09-18 21:56:55 -0300

    Add initial migration and model definition for Produto

--- app_samas/models.py
+++ app_samas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    quantidade_estoque = models.IntegerField(default=0)
+
+def __str__(self):
+    return self.nome
